5907c2a6d0b4164045ef35b7907bfacfc0217af0
cs-joy <mdzahangiralam1001@gmail.com>
2025-11-23 12:16:55 +0600

    basic - response model_return type

--- response_model_return_type.py
+++ response_model_return_type.py
@@ -212,17 +212,201 @@ async def get_teleport() -> RedirectResponse:
 
 # Invalid Return Type Annotations
 # source: https://fastapi.tiangolo.com/tutorial/response-model/#invalid-return-type-annotations
-
+'''
 @app.get('/invalid-return-type-annotations')
 async def test(teleport: bool = False) -> Response | dict:
     if teleport:
         return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     return {"message": "Here's your interdimensional portal."}
-
+'''
 # ... this failes because the type annotation is not a Pydantic type and is not just a single `Response` class or subclass, it's a union (any of the two)
 # between a `Response` and a `dict`.
 
 # Disable Response Model
-# ....
-# TODO
+# Continuing from the example above, we might not want to have default data validation, documentation, filtering etc. that is performed by FastAPI.
+# But we might want to still keep the return type annotation in the function to get the support from tools like editors and type checkers (e.g., mypy)
+
+# In this case, we can disable the response model generation by setting `response_model=None`
+
+@app.get('/disable-response-model/', response_model=None)
+async def disable_response_model(query_params: bool = False) -> Response | dict:
+    if query_params:
+        return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    return {"message": "Here's your interdimensional portal."}
+
+# This will make FastAPI skip response model generation and that way we can have any return type annotations we need withot it affecting our FastAPI application.
+
+'''
+# this code block will generate an error
+
+@app.get('/disable-response-model/')
+async def disable_response_model(query_params: bool = False) -> Response | dict:
+    if query_params:
+        return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    return {"message": "Here's your interdimensional portal."}
+'''
+
+# Response Model encoding parameters
+class Box(BaseModel):
+    id: int
+    description: str | None = None
+    width: float
+    height: float
+    tags: list[str] = []
+
+boxes = {
+    "A": {"id": 1, "width": 2.84, "height": 3.72},
+    "B": {"id": 2, "description": "Description Bar", "width": 3.95, "height": 6.23, "tags": ["tag1", "tag2"]},
+    "C": {"id": 3, "description": None, "width": 8.94, "height": 10.5, "tags": []},
+}
+
+@app.get('/response-model-encoding-parameters/{box_id}', response_model=Box, response_model_exclude_unset=True)
+async def view_square(box_id: str):
+    return boxes[box_id]
+
+# but we might want to omit them from the result if there were not actually stored.
+# for example, if we have models with many optional attributes in a NonSQL database, but we don't want to send very long JSOn responses full of default values.
+
+# Solution: User the `response_model_exclude_unset` parameter, value as `True`
+# `response_model_exclude_unset` -> this is a path operatin decorator
+
+'''
+and those default values won't be included in the response, only the values actually set.
+
+So, if you send a request to that path operation for the item with ID `A`, the response (not including default values) will be:
+
+```
+{
+    "id": 1,
+    "width": 2.84,
+    "height": 3.72
+}
+```
+'''
+# note: FastAPI uses Pydantic model's `.dict()` with it's 
+# exclude_unset: https://docs.pydantic.dev/1.10/usage/exporting_models/#modeldict
+# to achieve this.
+
+# We can also use:
+    # response_model_exclude_defaults=True
+    # response_model_exclude_none=True
+
+
+# Data with values for fields with defaults
+# # But if our data has values for the model's fields with defaults values, like the box with ID `B`:
+
+'''
+{
+    "id": 2, 
+    "description": "Description Bar", 
+    "width": 3.95, 
+    "height": 6.23, 
+    "tags": [
+        "tag1", 
+        "tag2"
+    ]
+}
+'''
+
+# Data with same value as the defaults
+# If the data has the same values as the default ones, like the box with ID `C`:
+'''
+{
+    "id": 3, 
+    "description": None, 
+    "width": 8.94, 
+    "height": 10.5, 
+    "tags": []
+}
+'''
+
+# FastAPI(Pydantic) is smart enough to realize that, even though `description`, `tags` have the same values as the defaults,
+# they were set explicitly (instead of taken from the defaults).
+
+# So, they will be included in the JSON response.
+
+
+# `response_model_include` and `response_model_exclude`
+# We can also use the path operation decorator parameters `response_model_include` and `response_model_exclude`.
+
+# They take a `set` of `str` with the name of the attributes to include (omitting the rest) or to exclude (including the rest).
+
+# this can be used a quick shorcut if we have only one pydanctic model 
+# and want to remove some data from the output.
+
+
+'''
+note: BUT, it is still recommended to use the ideas above, using multiple classes, instead of these parameters.
+
+This is because the JSON Schema generated in our app's OpenAPI (and the docs) will still be the one for the complete model, even if we use `response_model_include` or `response_model_exclude` to omit some attributes
+
+This also applies to `response_model_by_alias` that works similarly.
+'''
+
+class Adapter(BaseModel):
+    name: str
+    description: str | None = None
+    model: str
+    price: float
+    tax: float = 10.5
+
+items = {
+    "X": {"name": "Sumsung Montior Adapter 12v", "model": "Sam25", "price": 32.12},
+    "Y": {"name": "Mi Adapter", "description": "description box", "model": "V47", "price": 12.345, "tax": 10.5},
+    "Z": {
+        "name": "Nokie Charger",
+        "description": "cell phone charger",
+        "model": "e843",
+        "price": 34.12,
+        "tax": 10.5,
+    },
+}
+
+@app.get(
+    '/adapter/include/{item_id}/name', 
+    response_model=Adapter,
+    response_model_include={"name", "description"},
+)
+async def read_item_name(item_id: str):
+    return items[item_id]
+
+
+@app.get(
+    '/adapter/exclude/{item_id}/name',
+    response_model= Adapter,
+    response_model_exclude={"tax"},
+)
+async def read_item_public_data(item_id: str):
+    return items[item_id]
+
+# note:
+# the syntax {"name", "description"} creates a `set` with those two values.
+# it is equivalent to `set(["name", "description"])`
+
+# Using `list` s instead of set s
+# if we forgot to use a `set` and use `list` or `tuple` instead FastAPI will still convert it to a `set` and it will work correcly:
+
+
+@app.get(
+    '/set-list/items/{item_id}/name',
+    response_model= Adapter,
+    response_model_include=["name", "description"],
+)
+async def view_item_name(item_id: str):
+    return items[item_id]
+
+@app.get(
+    '/set-list/items/{item_id}/public',
+    response_model=Adapter, 
+    response_model_exclude=["tax"],
+)
+async def view_item_public_data(item_id: str):
+    return items[item_id]
+
+# # # # # # # # # 
+
+# Recap::::
+    # Use the path operation decorator's parameter `response_model` to define response models and especially to ensure private data is filtered out.
+
+    # Use `response_model_exclude_unset` to return only the values explicitly set 
 
