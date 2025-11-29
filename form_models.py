b18bc963e7b9d18f7a8b91d2e2445a8ae6e9bfc5
cs-joy <mdzahangiralam1001@gmail.com>
2025-11-29 08:44:38 +0600

    basic - form models

--- form_models.py
+++ form_models.py
@@ -24,3 +24,23 @@ async def login(data: Annotated[FormData, Form()]):
     return data
 
 
+# Forbid Extra Form Fields
+# # In some special uses cases (probably not very common), we might want to restrict the form fields to only those declared in the Pydantic model. and forbid any extra fields
+
+# to do that,
+class FormInfo(BaseModel):
+    username: str
+    password: str
+    model_config = {"extra": "forbid"}
+
+@app.post('/login/forbid/')
+async def sign_in(data: Annotated[FormInfo, Form()]):
+    return data
+
+
+
+
+
+
+
+
